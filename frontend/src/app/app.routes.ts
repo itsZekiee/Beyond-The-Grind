212f4109f1dab88aa4b0780ae9f3b5e2379cb556
itsZekiee <habee2004@gmail.com>
2026-03-13 17:09:52 +0800

    refractor

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -10,4 +10,5 @@ export const routes: Routes = [
   { path: 'admin-login', loadComponent: () => import('./pages/admin-login').then(m => m.AdminLoginComponent) },
   { path: 'admin', loadComponent: () => import('./pages/admin').then(m => m.AdminComponent) },
   { path: 'article/:id', loadComponent: () => import('./pages/article-details').then(m => m.ArticleDetailsComponent) },
+  { path: 'get-password-reset', loadComponent: () => import('./pages/get-password-reset').then(m => m.GetPasswordResetComponent) },
 ];
---
+++ frontend/src/app/pages/get-password-reset.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-password-reset',
+  template: `
+    <div>
+      <h1>Get Password Reset</h1>
+    </div>
+  `,
+  standalone: true,
+})
+export class GetPasswordResetComponent {}
+
